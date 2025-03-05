55a377d9599239881aa3e03692a92169764a52ee
Ahmed <an1977304@gmail.com>
2025-03-05 19:40:35 +0200

    Create Custom AppBar In Notes View

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -5,11 +5,20 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Column(children: <Widget>[CustomAppBarWidget()]);
+  }
+}
+
+class CustomAppBarWidget extends StatelessWidget {
+  const CustomAppBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
       children: <Widget>[
-        Row(
-          children: <Widget>[
-          ],
+        Text(
+          "Notes",
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
         ),
       ],
     );
