e9fff6b766817dd1c09d8dbdf3f178de1a3283f3
Ahmed <an1977304@gmail.com>
2025-03-05 22:56:37 +0200

    Create Custom Icon Button

--- lib/views/widgets/custom_app_bar_widget.dart
+++ lib/views/widgets/custom_app_bar_widget.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_icon_button.dart';
 
 class CustomAppBarWidget extends StatelessWidget {
   const CustomAppBarWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Text(
-          "Notes",
-          style: TextStyle(
-            fontSize: 32, 
-            fontWeight: FontWeight.bold
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            "Notes",
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
           ),
-          
-        ),
-      ],
+          CustomIconButton(),
+        ],
+      ),
     );
   }
 }
---
+++ lib/views/widgets/custom_icon_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomIconButton extends StatelessWidget {
+  const CustomIconButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.search, size: 28),
+      ),
+    );
+  }
+}
